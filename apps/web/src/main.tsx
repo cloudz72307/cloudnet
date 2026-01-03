6dd15e805cc1224b17979d7cdb99348643a72126
cloudz72307 <chaceroman684@gmail.com>
2026-01-03 02:57:55 -0500

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CloudNetLayout } from "./CloudNetLayout";
+import CloudNetLayout from "./CloudNetLayout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
